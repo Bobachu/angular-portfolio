2fdeda326cf540f9abc7b67e3c33b71678cbaea2
Chewie <43552746+Bobachu@users.noreply.github.com>
2019-08-08 17:00:44 -0700

    portfilio complete and email form added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './shared/containers/home/home.component';
+import { AboutComponent } from './shared/containers/about/about.component';
+import { PortfolioComponent } from './shared/containers/portfolio/portfolio.component';
+import { ContactComponent } from './shared/containers/contact/contact.component';
+import { GuestbookComponent } from './shared/containers/guestbook/guestbook.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'resume', component: AboutComponent},
+  {path: 'portfolio', component: PortfolioComponent},
+  {path: 'contact', component: ContactComponent},
+  {path: 'guestbook', component: GuestbookComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,35 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {HttpClientModule} from '@angular/common/http';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './shared/components/header/header.component';
+import { FooterComponent } from './shared/components/footer/footer.component';
+import { AboutComponent } from './shared/containers/about/about.component';
+import { ContactComponent } from './shared/containers/contact/contact.component';
+import { HomeComponent } from './shared/containers/home/home.component';
+import { PortfolioComponent } from './shared/containers/portfolio/portfolio.component';
+import { GuestbookComponent } from './shared/containers/guestbook/guestbook.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    AboutComponent,
+    ContactComponent,
+    HomeComponent,
+    PortfolioComponent,
+    GuestbookComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    ReactiveFormsModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
